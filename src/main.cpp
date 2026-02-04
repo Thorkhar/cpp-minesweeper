dd3d97c6d19f949c533345800672339144b90d6f
Thorkhar <test@test.com>
2026-02-04 19:20:04 +0100

    move texture loading to class + header file

--- src/main.cpp
+++ src/main.cpp
@@ -2,38 +2,8 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include "class/Field.h"
+#include "class/Textures.h"
 
-struct Textures {
-    sf::Texture tileMine;
-    sf::Texture tileUnknown;
-    sf::Texture tileFlag;
-    sf::Texture tileEmpty;
-    sf::Texture tileExploded;
-    sf::Texture tileOne;
-    sf::Texture tileTwo;
-    sf::Texture tileThree;
-    sf::Texture tileFour;
-    sf::Texture tileFive;
-    sf::Texture tileSix;
-    sf::Texture tileSeven;
-    sf::Texture tileEight;
-
-    void load() {
-        tileMine.loadFromFile("../assets/TileMine.png");
-        tileUnknown.loadFromFile("../assets/TileUnknown.png");
-        tileFlag.loadFromFile("../assets/TileFlag.png");
-        tileEmpty.loadFromFile("../assets/TileEmpty.png");
-        tileExploded.loadFromFile("../assets/TileExploded.png");
-        tileOne.loadFromFile("../assets/Tile1.png");
-        tileTwo.loadFromFile("../assets/Tile2.png");
-        tileThree.loadFromFile("../assets/Tile3.png");
-        tileFour.loadFromFile("../assets/Tile4.png");
-        tileFive.loadFromFile("../assets/Tile5.png");
-        tileSix.loadFromFile("../assets/Tile6.png");
-        tileSeven.loadFromFile("../assets/Tile7.png");
-        tileEight.loadFromFile("../assets/Tile8.png");
-    }
-};
 
 int main() {
     sf::RenderWindow window(sf::VideoMode({800, 600}), "Minesweeper");
